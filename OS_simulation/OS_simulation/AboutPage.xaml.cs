4299eaba9184473cfeb08ce3d48660dd7883657e
Shashwat422 <b517040@iiit-bh.ac.in>
2020-04-23 01:54:22 +0530

    Upload version 1.1

---
+++ OS_simulation/OS_simulation/AboutPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OS_simulation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+			this.Title = "About";
+		}
+
+		//public ICommand UrlCommand => new Command<string>(OpenLink);
+		//async void OpenLink(string url)
+		//{
+		//	try
+		//	{
+		//		var link = new Uri(url);
+		//		await Browser.OpenAsync(link, BrowserLaunchMode.SystemPreferred);
+		//	}
+		//	catch
+		//	{
+		//		await DisplayAlert("Error", "Invlid Url", "OK");
+		//	}
+		//}
+
+		async private void OpenLinkedInUrl(object sender, EventArgs e)
+		{
+			try
+			{
+				string url = "https://www.linkedin.com/in/shashwat-b-5871a3175";
+				var link = new Uri(url);
+				await Browser.OpenAsync(link, BrowserLaunchMode.SystemPreferred);
+			}
+			catch
+			{
+				await DisplayAlert("Error", "Invlid Url", "OK");
+			}
+		}
+
+		async private void OpenGithubUrl(object sender, EventArgs e)
+		{
+			try
+			{
+				string url = "https://github.com/Shashwat422/Xamarin_projects/tree/master/OS_simulation";
+				var link = new Uri(url);
+				await Browser.OpenAsync(link, BrowserLaunchMode.SystemPreferred);
+			}
+			catch
+			{
+				await DisplayAlert("Error", "Invlid Url", "OK");
+			}
+		}
+	}
+}
